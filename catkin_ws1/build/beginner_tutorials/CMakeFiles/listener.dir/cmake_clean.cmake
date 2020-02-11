fec95e57a44655edb79a3b9194e959543cdc46e6
hahaha_rzq <2362847697@qq.com>
2020-02-11 14:44:34 +0800

    ros topic program.

---
+++ catkin_ws1/build/beginner_tutorials/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listener.dir/src/listener/listener.cpp.o"
+  "/home/hahaha/workspace/ros_program/catkin_ws1/devel/lib/beginner_tutorials/listener.pdb"
+  "/home/hahaha/workspace/ros_program/catkin_ws1/devel/lib/beginner_tutorials/listener"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws1/build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker/talker.cpp.o"
+  "/home/hahaha/workspace/ros_program/catkin_ws1/devel/lib/beginner_tutorials/talker.pdb"
+  "/home/hahaha/workspace/ros_program/catkin_ws1/devel/lib/beginner_tutorials/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
